a851d1b7479a9e7a4bc8cd2eb2ce0679c2cbf771
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-07-06 15:48:25 -0500

    MemoryDatabase.kt added, MemoryTypeConverters.kt added, build.gradle added Room import, Memory.kt modded to use database

--- PetHeart/app/src/main/java/com/example/petheart/Memory.kt
+++ PetHeart/app/src/main/java/com/example/petheart/Memory.kt
@@ -1,8 +1,12 @@
 package com.example.petheart
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Memory(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
     var date: Date = Date(),
