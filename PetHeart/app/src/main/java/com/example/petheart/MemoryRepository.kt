3d5fe1258de5ef3a7f047c31109d3e786d53eaa0
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-07-06 16:24:37 -0500

    MemoryRepository.kt added Dao functions to repo

--- PetHeart/app/src/main/java/com/example/petheart/MemoryRepository.kt
+++ PetHeart/app/src/main/java/com/example/petheart/MemoryRepository.kt
@@ -1,8 +1,25 @@
 package com.example.petheart
 
 import android.content.Context
+import androidx.room.Room
+import com.example.petheart.database.MemoryDatabase
+import java.util.*
+
+private const val DATABASE_NAME = "memory-database"
 
 class MemoryRepository private constructor(context: Context) {
+
+    private val database: MemoryDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        MemoryDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val memoryDao = database.memoryDao()
+
+    fun getMemories(): List<Memory> = memoryDao.getMemories()
+
+    fun getMemory(id: UUID): Memory? = memoryDao.getMemory(id)
 
     companion object {
         private var INSTANCE: MemoryRepository? = null
